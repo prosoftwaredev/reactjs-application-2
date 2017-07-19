b0d5e47c9f572b10f902c069b7f1a078479650ae
realsoftdev <devfinal310@gmail.com>
2017-07-19 12:34:03 -0400

    installed passport-jwt and changed the mongodb url

--- server/config.js
+++ server/config.js
@@ -1,5 +1,5 @@
 const config = {
-  mongoURL: process.env.MONGO_URL || 'mongodb://localhost:27017/mern-starter',
+  mongoURL: process.env.MONGO_URL || 'mongodb://localhost:27017/knack_image',
   port: process.env.PORT || 8000,
   secretKey: 'rhettsmitttoken'
 };
